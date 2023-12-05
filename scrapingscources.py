c151e42dd11944b19d065c4e6ea786c4b061d4c2
Alistair Simmons <alistair.simmons@duke.edu>
2023-12-05 13:03:45 -0500

    changes

--- scrapingscources.py
+++ scrapingscources.py
@@ -1,12 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import requests
 from bs4 import BeautifulSoup as bs
+#from urllib import urlopen as ureq
+import time
 
-r = requests.get('https://ui.adsabs.harvard.edu/abs/2023arXiv230508746L/citations')
-content = r.text
-soup = bs(content, 'lxml')
+TIMEOUT = 60
 
-box = soup.find('ul', class_='results-list')
-tags = box.find_all('li') # Limit the search to within the 'box' variable
+chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.default_content_setting_values.notifications": 2}
+chrome_options.add_experimental_option("prefs", prefs)
+driver = webdriver.Chrome(options=chrome_options)
 
-for tag in tags:
-  print(tag.text)
+url = 'https://ui.adsabs.harvard.edu/abs/2023arXiv230508746L/references'
+driver.get(url)
+try:
+  elem = WebDriverWait(driver, TIMEOUT).until(
+    EC.presence_of_element_located((By.CLASS_NAME, 'results-list'))
+  )
+finally:
+   html = driver.page_source
+   #print(html)
+   soup = bs(html, 'html.parser')
+   if soup != None:
+    references = soup.find_all('a', class_='abs-redirect-link')
+    print(references)
+    titles = soup.find_all('h3', class_='s-results-title')
+    #
+    #print(titles)
+    #dates = #contained within the link to the abstract -- #abs/2023arXiv230211529P/abstract
+    authors = soup.find_all('li', class_='author')
+    #print(authors)
+
+
+
